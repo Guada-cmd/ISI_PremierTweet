82039194a876928877fbb27bcc22be5fae9f89e9
Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
2021-04-11 13:43:57 +0200

    Prueba

--- imdb.py
+++ imdb.py
@@ -2,42 +2,59 @@ import json, requests
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = "HBE7m9FTfdR7oC5awdcuDTS66"
-consumer_secret = "hHDJBblupuLHephjNzxi9CKVwCGHqAAVec8sgsl4zX4BbtV85H"
-access_token = "1369335372094840838-Kur6OjM5AIqBj9xniAnz4B98sEOgB7"
-access_token_secret = "8SAyCy20rThI0ZBtQDNgoB4Wvp8nkSD9svIYBPFWVBMj8"
+# Constantes
 
-url = "https://imdb-api.com/en/API/InTheaters/k_j8vk26rm"
-response = requests.get(url)
-data = json.loads(response.text)
-peliculas = []
+CONSUMER_KEY = "HBE7m9FTfdR7oC5awdcuDTS66"
+CONSUMER_SECRET = "hHDJBblupuLHephjNzxi9CKVwCGHqAAVec8sgsl4zX4BbtV85H"
+ACCESS_TOKEN = "1369335372094840838-Kur6OjM5AIqBj9xniAnz4B98sEOgB7"
+ACCESS_TOKEN_SECRET = "8SAyCy20rThI0ZBtQDNgoB4Wvp8nkSD9svIYBPFWVBMj8"
+URL = "https://imdb-api.com/en/API/InTheaters/k_j8vk26rm"
 
 
-f = open("data.json", "r")
-content = f.read()
-jsondecoded = json.loads(content)
+def obtenerListaPeliculas():
 
-for entity in jsondecoded["items"]:
-    peliculas.append(entity["title"])
+    lista_peliculas = []
 
-    #if entity["year"] == '2020':
-        #result.append(entity["title"]+" "+entity["year"])
-        #print(entity["title"]+" "+entity["year"])
-        #print(entity["title"])
-	
+    response = requests.get(URL)    
+    data = json.loads(response.text)
+    
+    file = open("data.json", "r")
+    content = file.read()
+    json_decoded = json.loads(content)
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    for entity in json_decoded["items"]:
+        lista_peliculas.append(entity["title"])
+
+    return lista_peliculas
+
+
+def main():
+
+    lista_peliculas = []
+
+    lista_peliculas = obtenerListaPeliculas()
+
+
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
  
-api = tweepy.API(auth)
+    api = tweepy.API(auth)
+
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error during authentication")
+
+    list_tweets = []
+
+    for i in (lista_peliculas):
+	    for tweet in tweepy.Cursor(api.search,q=i,count=100, lang="en", since="2017-04-03").items(20):
+		    print (tweet.created_at, tweet.text)
+           
+            
 
 try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
-    
-   
-for i in (peliculas):
-	for tweet in tweepy.Cursor(api.search,q=i,count=100, lang="en", since="2017-04-03").items():
-		print (tweet.created_at, tweet.text)
+    main()
+except KeyboardInterrupt:
+    pass    
